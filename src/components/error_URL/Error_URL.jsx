66fad594a5f335dfef349a6178bb6ed84e5c796b
Witty Umosung <64681844+Pukkawit@users.noreply.github.com>
2024-05-29 19:18:07 +0100

    blog-post-task

--- src/components/error_URL/Error_URL.jsx
+++ src/components/error_URL/Error_URL.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./error_url.scss";
 const Error_URL = () => {
   return (
     <div>
-      <h3
-        style={{
-          letterSpacing: "unset",
-          textTransform: "none",
-          fontSize: "2rem",
-          textAlign: "center",
-        }}
-      >
-        Oops! Looks like you mistyped the url. Click this instead{" "}
-        <Link to="/">Home</Link>
-      </h3>
+      <div className="error-div">
+        <h1>:/</h1>
+        <h3>Oops! Looks like this url is invalid...</h3>
+        <p>Error 404 - the requested URL was not found on the server.</p>
+        <Link to="/">Return to the Homepage</Link>
+      </div>
     </div>
   );
 };
